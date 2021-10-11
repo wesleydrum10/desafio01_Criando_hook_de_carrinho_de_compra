9d32c424f5e11fef4ad1cefa92bf1102a807eaa6
WESLEY CARLOS DE OLIVEIRA <you@example.comwesley@worc.com.br>
2021-10-11 11:21:45 -0300

    Renderizando produtos

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,7 +23,7 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
     // if (storagedCart) {
     //   return JSON.parse(storagedCart);
